6c870ba63a47c0f59fbeafff129f4e692cb41f12
Matt Griswold <grizz@20c.com>
2018-09-21 14:17:21 +0000

    fix install

--- setup.py
+++ setup.py
@@ -1,25 +1,31 @@
 
 from setuptools import setup, find_packages
 
-version = open('config/VERSION').read().strip()
-requirements = open('config/requirements.txt').read().split("\n")
-test_requirements = open('config/requirements-test.txt').read().split("\n")
+def read_file(name):
+    with open(name) as fobj:
+        return fobj.read().strip()
+
+LONG_DESCRIPTION = read_file("README.md")
+VERSION = read_file("config/VERSION")
+REQUIREMENTS = read_file("config/requirements.txt").split('\n')
+TEST_REQUIREMENTS = read_file("config/requirements-test.txt").split('\n')
+
 
 setup(
     name='facsimile',
-    version=version,
+    version=VERSION,
     author='20C',
     author_email='code@20c.com',
     description='',
-    long_description=open('README.txt').read(),
+    long_description=LONG_DESCRIPTION,
     license='LICENSE.txt',
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: Apache Software License',
     ],
     url='https://github.com/20c/facsimile',
-    install_requires=requirements,
-    test_requires=test_requirements,
+    install_requires=REQUIREMENTS,
+    test_requires=TEST_REQUIREMENTS,
     packages=find_packages(),
     package_data={'facsimile': ['script/*.sh']},
     scripts=['facsimile/bin/facs', 'facsimile/bin/version_bump_dev', 'facsimile/bin/version_merge_release'],
